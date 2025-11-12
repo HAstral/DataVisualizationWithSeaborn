86d8c18c8bf5bf566bcd32edf86187a71ce6eff3
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-12 19:12:25 +0630

    new py added

---
+++ Replot2.py
@@ -0,0 +1,19 @@
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+# Load student data
+student_data = pd.read_csv('student-alcohol-consumption.csv')
+
+# Adjust further to add subplots based on family support
+sns.relplot(x="G1", y="G3", 
+            data=student_data,
+            kind="scatter", 
+            col="schoolsup",
+            col_order=["yes", "no"],
+            row="famsup",
+            row_order=['yes','no'])
+
+# Show plot
+plt.show()
