089bc9aa2f5aba4a0ff4c1ea2141f685b439a0ff
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-13 22:09:12 +0630

    new py added

---
+++ pointplot1.py
@@ -0,0 +1,14 @@
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+student_data=pd.read_csv('student-alcohol-consumption.csv')
+# Remove the lines joining the points
+sns.catplot(x="famrel", y="absences",
+			data=student_data,
+            kind="point",
+            capsize=0.2,join=False)
+            
+# Show plot
+plt.show()
