513e2bc34c53912f0be1c923c462fc4745c84842
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-13 21:00:41 +0630

    new py added

---
+++ catplot1.py
@@ -0,0 +1,12 @@
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+survey_data=pd.read_csv('young-person-survey-responses.csv')
+# Create count plot of internet usage
+sns.catplot(x='Internet usage',data=survey_data,kind='count')
+
+
+# Show plot
+plt.show()
