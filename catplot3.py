ca77bd2cb69c35071e7fb592c2fcbc826d7002e6
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-13 21:09:49 +0630

    new py added

---
+++ catplot3.py
@@ -0,0 +1,20 @@
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+student_data=pd.read_csv('student-alcohol-consumption.csv')
+# List of categories from lowest to highest
+category_order = ["<2 hours", 
+                  "2 to 5 hours", 
+                  "5 to 10 hours", 
+                  ">10 hours"]
+
+# Turn off the confidence intervals
+sns.catplot(x="study_time", y="G3",
+            data=student_data,
+            kind="bar",
+            order=category_order,ci=None)
+
+# Show plot
+plt.show()
