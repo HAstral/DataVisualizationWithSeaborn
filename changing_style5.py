432c62cb8d95d1372be4ae2e333fcde4f5564265
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-14 21:25:39 +0630

    new py added

---
+++ changing_style5.py
@@ -0,0 +1,19 @@
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+mpg=pd.read_csv('mpg.csv')
+# Create point plot
+sns.catplot(x="origin", 
+            y="acceleration", 
+            data=mpg, 
+            kind="point", 
+            join=False, 
+            capsize=0.1)
+
+# Rotate x-tick labels
+plt.xticks(rotation=90)
+
+# Show plot
+plt.show()
